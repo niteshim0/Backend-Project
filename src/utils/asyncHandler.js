9ebfadc47d708fd1d836ab7b71422a1076adab43
niteshim0 <niteshim111@gmail.com>
2023-12-12 17:01:41 +0530

    correction in asyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,33 +1,6 @@
-const asyncHandler = (requestHandler)=> {
-  (req,res,next)=>{
-    Promise.resolve(requestHandler(req,res,next))
-    .catch((err)=>next(err))
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+      .catch((err) => next(err))
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//Higher Order Function can take function as argument as well as return them
-//next is used for middlewares 
-//Way I(try-catch statement) of using some common function as utitlities and combine them in certain wrappers
-/*const asyncHandler = (requestHandler)=> async(req,res,next)=>{
-  try {
-     await requestHandler(req,res,next);
-  } catch (err) {
-    res.status(err.code||500).json({
-      success:false,
-      message:err.message
-
-    })
-  }
-}*/
-
