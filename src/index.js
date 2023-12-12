2bcf04a2fde47653253057d2889769eb47e406d9
niteshim0 <niteshim111@gmail.com>
2023-12-12 15:09:32 +0530

    connecting database in MERN,how and why

--- src/index.js
+++ src/index.js
@@ -1,25 +1,93 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Approach I to connect to Database
 //Everything in index file (not so professional)
 //Database is always in another continent so make sure to use try and catch block alongwith async and await while connecting to database
-const app = express();
-;(async()=>{
-  try{
-    mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error",(error)=>{
-      console.log("ERROR: ",error);
-      throw error;
-    })
+// const app = express();
+// ;(async()=>{
+//   try{
+//     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error",(error)=>{
+//       console.log("ERROR: ",error);
+//       throw error;
+//     })
 
-    app.listen(process.env.PORT,()=>{
-      console.log(`App is listening on 
-      ${process.env.PORT}`)
-    })
-  }catch(error){
-    console.error("ERROR: ",error)
-    throw error;
-  }
+//     app.listen(process.env.PORT,()=>{
+//       console.log(`App is listening on 
+//       ${process.env.PORT}`)
+//     })
+//   }catch(error){
+//     console.error("ERROR: ",error)
+//     throw error;
+//   }
 
-})()
+// })()
